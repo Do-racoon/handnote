bbca263888f7c72cb279e5ec37fb4ca3aab467f7
Jeong-Hyun-Su <mses1572@naver.com>
2020-08-07 22:29:58 +0900

    Spring init

---
+++ handnote_server/src/test/java/com/capella/handnote/HandnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capella.handnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
